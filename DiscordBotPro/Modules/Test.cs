d60b269be14295454e7314e710950a4e9762647c
X <X@DESKTOP-4FKPNKE>
2017-05-25 23:59:51 +0200

    Add project files.

---
+++ DiscordBotPro/Modules/Test.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace DiscordBotPro.Modules
+{
+    public class Test : ModuleBase<SocketCommandContext>
+    {
+        [Command("test")]
+        public async Task Testt()
+        {
+
+            // Load something here :)
+        }
+        }
+    }
+}
---
+++ DiscordBotPro/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace DiscordBotPro
+{
+
+    public class Program : ModuleBase<SocketCommandContext>
+    {
+
+        static void Main(string[] args)
+        => new Program().StartAsync().GetAwaiter().GetResult();
+
+        
+
+        private DiscordSocketClient _client;
+
+        private CommandHandler.CommandHandler _handler;
+
+        public async Task StartAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            new CommandHandler.CommandHandler();
+
+            string api = File.ReadLines(@"config.txt").Skip(1).Take(1).First();
+
+            await _client.LoginAsync(TokenType.Bot, api);
+
+            await _client.StartAsync();
+
+            _handler = new CommandHandler.CommandHandler();
+
+            await _handler.InitializeAsync(_client);
+
+
+            Console.WriteLine("DiscordBnsBot");
+            Console.WriteLine(" ");
+            Console.WriteLine("You are connecting to Discord using api: " + api);
+            Console.WriteLine(" ");
+            Console.WriteLine("You can change api settings inside config.txt");
+
+
+
+
+            await Task.Delay(-1);
+
+
+
+        }
+    }
+}
